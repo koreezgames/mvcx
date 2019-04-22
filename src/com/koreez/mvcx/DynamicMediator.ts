287af030df44e761dc4870f08b13b1e6a75b4783
saqsun <sargsyan.sargis.89@gmail.com>
2019-04-22 18:34:36 +0400

    minor cleanup

--- src/com/koreez/mvcx/DynamicMediator.ts
+++ src/com/koreez/mvcx/DynamicMediator.ts
@@ -2,10 +2,6 @@ import { Mediator } from "./Mediator";
 import { IDynamicView } from "./View";
 
 export class DynamicMediator<T extends IDynamicView> extends Mediator<T> {
-    constructor() {
-        super();
-    }
-
     protected get mediatorName(): string {
         return this.view.uuid;
     }
